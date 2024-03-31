d9d59fb967d34b464e688534efedfab6302347b4
prashannastha7 <prashannabdrshrestha711@gmail.com>
2024-03-31 07:29:40 +0545

    initial commit

---
+++ StudentPortal/StudentPortal/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StudentPortal.Data
+{
+    public class ApplicationDbContext: DbContext 
+    {
+        public ApplicationDbContext()
+        {
+            
+        }
+    }
+}

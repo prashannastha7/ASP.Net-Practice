b0b1fe9ef5bb478d1999d0f079f52759bccff08c
prashannastha7 <prashannabdrshrestha711@gmail.com>
2024-04-09 22:04:48 +0545

    Added Models and Controller

---
+++ NpWalkz/NpWalks/NpWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NpWalks.API.Controllers
+{
+    //https://localhost:portnumber/api/students
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        //Get: https://localhost:portnumber/api/students
+
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentNames = new string[]
+            {
+                "Ram", "Shyam", "Sita", "Krishna", "Radha"
+            };
+
+            return Ok(studentNames);
+        }
+    }
+}
---
+++ NpWalkz/NpWalks/NpWalks.API/Models/Domain/Walk.cs
@@ -0,0 +1,15 @@
+﻿namespace NpWalks.API.Models.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public required string  Description{ get; set; }
+        public string? WalkImageUrl { get; set; } //? is used to make nullable
+        public Guid DifficultyId{ get; set; }
+        public Guid RegionId{ get; set; }
+    }
+
+    //Navigation properties
+
+}

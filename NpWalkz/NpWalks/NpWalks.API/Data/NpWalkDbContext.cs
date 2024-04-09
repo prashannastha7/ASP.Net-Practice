ae36828ee4ee9456d4a1d8044c51d5e96c59d46d
prashannastha7 <prashannabdrshrestha711@gmail.com>
2024-04-09 23:24:10 +0545

    Added EFCore

---
+++ NpWalkz/NpWalks/NpWalks.API/Data/NpWalkDbContext.cs
@@ -0,0 +1,19 @@
+﻿//DbContext class is a bridge betw model and db, interctaing with db and performs CRUD on tables
+
+using Microsoft.EntityFrameworkCore;
+using NpWalks.API.Models.Domain;
+
+namespace NpWalks.API.Data
+{
+    public class NpWalkDbContext: DbContext
+    {
+        public NpWalkDbContext(DbContextOptions dbContextOptions): base(dbContextOptions) 
+        { 
+
+        }
+        //DbSet represent a collection of entities in the db
+        public DbSet<Difficulty> Difficulties{ get; set; }
+        public DbSet<Region> Regions{ get; set; }
+        public DbSet<Walk> Walks{ get; set; }
+    }
+}

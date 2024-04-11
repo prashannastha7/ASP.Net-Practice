4e9fc25d1efb49e99e32cdb3771c368b769ef37d
prashannastha7 <prashannabdrshrestha711@gmail.com>
2024-04-11 18:48:38 +0545

    Added DTOs

--- NpWalkz/NpWalks/NpWalks.API/Controllers/RegionsController.cs
+++ NpWalkz/NpWalks/NpWalks.API/Controllers/RegionsController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using NpWalks.API.Data;
 using NpWalks.API.Models.Domain;
+using NpWalks.API.Models.DTO;
 
 namespace NpWalks.API.Controllers
 {
@@ -23,27 +24,43 @@ namespace NpWalks.API.Controllers
         public IActionResult GetAll()
         {
             /*  without db to check in postman write this
-             var regions = new List<Region>
-             {
-                 new Region
-                 {
-                     Id = Guid.NewGuid(),
-                     Name = "Patan Region",
-                     Code = "Pt",
-                     RegionImageUrl = "https://www.istockphoto.com/photo/patan-durbar-square-gm514651173-47651120"
-                 },
-                  new Region
-                 {
-                     Id = Guid.NewGuid(),
-                     Name = "Pokhara Region",
-                     Code = "Pkr",
-                     RegionImageUrl = "https://www.shutterstock.com/image-photo/phewa-lake-nepal-132349601"
-                 }
-             };*/
+ var regions = new List<Region>
+ {
+     new Region
+     {
+         Id = Guid.NewGuid(),
+         Name = "Patan Region",
+         Code = "Pt",
+         RegionImageUrl = "https://www.istockphoto.com/photo/patan-durbar-square-gm514651173-47651120"
+     },
+      new Region
+     {
+         Id = Guid.NewGuid(),
+         Name = "Pokhara Region",
+         Code = "Pkr",
+         RegionImageUrl = "https://www.shutterstock.com/image-photo/phewa-lake-nepal-132349601"
+     }
+ };*/
+
+            // Get Data From Database - Domain models
+            var regionsDomain = dbContext.Regions.ToList();
 
 
-            var regions = dbContext.Regions.ToList();
-            return Ok(regions);
+            //Map Domian Modeks to DTOs
+            var regionDTO = new List<RegionDto>();
+            foreach (var region in regionsDomain) 
+            {
+                regionDTO.Add(new RegionDto()
+                {
+                    Id = region.Id,
+                    Name = region.Name,
+                    Code = region.Code,
+                    RegionImageUrl = region.RegionImageUrl
+                });
+             
+            }
+            //Return DTOs
+            return Ok(regionDTO);
         }
 
         //Get Single region: Get:https://localhost:portno/api/regions/{id}
@@ -53,13 +70,25 @@ namespace NpWalks.API.Controllers
 
         public IActionResult GetById([FromRoute]Guid id)
         {
-            var region = dbContext.Regions.Find(id);
+            //Get Region Domain Model from db
+            var regionDomain = dbContext.Regions.Find(id);
 
-            if (region == null)
+            if (regionDomain == null)
             {
                 return NotFound();
             }
-            return Ok(region);
+
+            //Map Region Domain Model to Region DTO
+
+            var regionDto = new RegionDto
+            {
+                Id = regionDomain.Id,
+                Name = regionDomain.Name,
+                Code = regionDomain.Code,
+                RegionImageUrl = regionDomain.RegionImageUrl
+
+            };
+            return Ok(regionDto);
         }
 
     }
---
+++ NpWalkz/NpWalks/NpWalks.API/Models/DTO/RegionDto.cs
@@ -0,0 +1,18 @@
+﻿// DTOs used : to not expose the names of domain,
+// helps to only exposed the limited data to client 
+// changes the model but the dtos reminds same so that we aren't breaking the contract btw client and API
+
+
+//This can have one or multiple properties of Domain Model
+
+
+namespace NpWalks.API.Models.DTO
+{
+    public class RegionDto
+    {
+        public Guid Id { get; set; }
+        public required string Code { get; set; }
+        public required string Name { get; set; }
+        public string? RegionImageUrl { get; set; } //? is used to make nullable
+    }
+}
